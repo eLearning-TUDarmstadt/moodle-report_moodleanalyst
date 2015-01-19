19e8782196d8b35435a8ae4d7af64e8da9e1db94
Steffen Pegenau <steffen.pegenau@gmail.com>
2015-01-19 16:23:23 +0100

    first running rest example on our alpha server with angular.js

--- js/angular.js
+++ js/angular.js
@@ -6,13 +6,26 @@
 
 var app = angular.module('overview', []);
 
-app.controller('overview', function() {
-        this.nothing = 0;
-});
-
-app.directive('overview', function() {
+app.directive('overview', function () {
     return {
         restrict: 'E',
-        templateUrl: '/overview.tpl.html'
+        templateUrl: '/report/moodleanalyst/html/overview.tpl.html'
     };
+});
+
+app.directive('courseinfo', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/report/moodleanalyst/html/courseinfo.tpl.html',
+        controller: [
+            '$http','$scope', function ($http, $scope) {
+                this.course = [];
+                $http.get('https://mdl-alpha.un.hrz.tu-darmstadt.de/report/moodleanalyst/rest/router.php/course/id/64')
+                        .success(function (result) {
+                            $scope.course = result.Records[0];
+                            console.log($scope.course);
+                        });
+            }],
+        controllerAs: 'ctrl'
+    }
 });
