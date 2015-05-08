385683af113b71e81f5c91bde94d5d62a8f8d293
Steffen Pegenau <steffen.pegenau@gmail.com>
2015-05-08 13:09:32 +0200

    Closes #49 - Removed unused string with illegal definition

--- lang/en/report_moodleanalyst.php
+++ lang/en/report_moodleanalyst.php
@@ -23,7 +23,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 $string['pluginname'] = 'Moodle AnalyST';
-$string['moodleanalyst'] = $string['pluginname'];
 $string['reportmoodleanalyst'] = 'reportmoodleanalyst';
 
 $string['parentcategory'] = 'Department';
