f316cb22fdfac84ca68efa51ea2cbb078043c723
GranatenArnold <kirsche@kirschmarmelade.de>
2015-08-04 12:05:50 +0200

    Closes #58

--- settings.php
+++ settings.php
@@ -24,6 +24,6 @@
  */
 
 defined('MOODLE_INTERNAL') || die;
-$ADMIN->add('reports', new admin_externalpage('reportmoodleanalyst', get_string('moodleanalyst', 'report_moodleanalyst'), $CFG->wwwroot."/report/moodleanalyst/index.php", 'report/moodleanalyst:view'));
+$ADMIN->add('reports', new admin_externalpage('reportmoodleanalyst', get_string('moodleanalyst', 'report_moodleanalyst'), "$CFG->wwwroot/report/moodleanalyst/index.php", 'report/moodleanalyst:view'));
 // no report settings
 $settings = null;
