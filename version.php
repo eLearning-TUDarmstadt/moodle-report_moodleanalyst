70eae4b5a1bf5f092850f0f7fafc63610a213e4b
Steffen Pegenau <steffen.pegenau@gmail.com>
2015-08-12 14:24:56 +0200

    Incremented version number because of the changed lang files

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015012100;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015081200;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012050100;         // Requires this Moodle version
 $plugin->component = 'report_moodleanalyst'; // Full name of the plugin (used for diagnostics)
 
